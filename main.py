1d7ad70372888d62ef89421dd269a5cb34bcc9ef
jabbarasfa3 <jabbarasfa3@gmail.com>
2023-01-09 22:30:04 +0500

    implemented multiply function

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ print("Multiply Function")
 int n1 = input("Enter first number")
 int n2 = input("Enter second number")
 int mul = n1* n2
-print("Result",mul)
-print("Divide")
-int n1=input("Enter dividend")
-int n2=input("Enter divisor")
-print("Result:", n1/n2)
